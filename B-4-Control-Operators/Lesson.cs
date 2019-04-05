7d82b8d663df0703ec146af246d26f1e10066fc7
rommoi <raman.maiseichuk@gmail.com>
2019-04-05 18:58:52 +0300

    Add project files.

---
+++ B-4-Control-Operators/Lesson.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Lesson_4._1
+{
+    partial class Lesson
+    {
+        public static void Switch_OddEven_Example()
+        {
+            var i = int.Parse(Console.ReadLine());
+
+            switch (i % 2)
+            {
+                case 0:
+                    Console.Write("Number is an even number");
+                    break;
+
+                case 1:
+                    Console.Write("Number is an odd number");
+                    break;
+            }
+        }
+
+        public static void Switch_LessGreater_Example()
+        {
+            int y = 2;
+            int x = 6;
+
+            switch (y - x)
+            {
+                case int r when r > 0:
+                    Console.WriteLine("y > x");
+                    break;
+
+                case int r when r < 0:
+                    Console.WriteLine("y < x");
+                    break;
+
+                case int r when r == 0:
+                    Console.WriteLine("y == x");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public static void CyclesExample()
+        {
+            #region for
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            #endregion
+
+            #region foreach
+
+            var numbers = new List<int> { 0, 1, 2, 3, 4 };
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            #endregion
+
+            #region while
+
+            int n = 0;
+
+            while (n < 5)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+
+            #endregion
+
+            #region do/while
+
+            int m = 0;
+
+            do
+            {
+                Console.WriteLine(m);
+                m++;
+            } while (m < 5);
+
+
+            #endregion
+        }
+
+        public static void ForVariations()
+        {
+            #region Multiple cycle variables
+
+            for (int i = 0, j = 10; i < j; i++, j--)
+                Console.WriteLine($"Элементы {i},{j}");
+
+
+            #endregion
+
+            #region Condition
+
+            var flag = true;
+            for (int i = 0; flag; i++)
+            {
+                if (i == 5) flag = false;
+            }
+
+            #endregion
+
+            #region Missed Iteration
+
+            for (int i = 0; i < 30;)
+            {
+                i += 3;
+                Console.WriteLine($"Значение i = {i}");
+            }
+
+            #endregion
+
+            #region Infinite loop
+
+            for (int i = 0; ;)
+            {
+                i++;
+                Console.WriteLine($"Значение i = {i}");
+                if (i == 1000) break;
+            }
+
+            #endregion
+
+            #region Without body
+
+            int sum = 0;
+            for (int i = 0; i < 10; sum += i++);
+
+            #endregion
+        }
+
+        public static void ForeachExample()
+        {
+            String word = "word";
+            foreach (Char letter in word)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+
+        public static void TransitionOperatorsExamples()
+        {
+            #region break vs continue
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 5) { break; }
+                Console.WriteLine(i);
+
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 5) { continue; }
+                Console.WriteLine(i);
+
+            }
+
+            #endregion
+
+            #region break vs return + throw
+
+            math(1, 3, "+");
+            math(1, 3, "~");
+
+            Console.WriteLine(math2(1, 3, "+"));
+            Console.WriteLine(math2(1, 3, "~"));
+
+            #endregion
+        }
+
+        public static void math(int a, int b, string operand)
+        {
+            var result = 0;
+
+            switch (operand)
+            {
+                case "+": result = a + b; break;
+                case "-": result = a - b; break;
+                case "*": result = a * b; break;
+
+                default: Console.WriteLine("Not supported operation"); break;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        public static int math2(int a, int b, string operand)
+        {
+            switch (operand)
+            {
+                case "+": return a + b;
+                case "-": return a - b;
+                case "*": return a * b;
+
+                default: throw new Exception("Not supported operation");
+            }
+        }
+
+
+    }
+}
+
---
+++ B-4-Control-Operators/Practice.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Lesson_4._1
+{
+    partial class Practice
+    {
+        //B4-P1/25. If_TimeOfDayGreeting
+        public static void B4_P1_25_If_TimeOfDayGreeting()
+        {
+            DateTime time = DateTime.N
+        }
+
+        //B4-P2/25. If_NumbersComparing
+        public static void B4_P2_25_If_NumbersComparing()
+        {
+        }
+
+        //B4-P3/25. If_PositiveNumbersComparing
+        public static void B4_P3_25_If_PositiveNumbersComparing()
+        {
+        }
+
+        //B4-P4/25. If_Akinator5Numbers
+        public static void B4_P4_25_If_Akinator5Numbers()
+        {
+            
+        }
+
+        //B4-P5/25. Switch_DayOfWeek
+        public static void B4_P5_25_Switch_DayOfWeek()
+        {
+        }
+
+        //B4-P6/25. Switch_GameNavigation
+        public static void B4_P6_25_Switch_GameNavigation()
+        {
+            
+        }
+
+        //B4-P7/25. For_10OddEven
+        public static void B4_P7_25_For_10OddEven()
+        {
+            
+        }
+
+
+        //B4-P8/25. For_3DevideNumbers
+        public static void B4_P8_25_For_3DevideNumbers()
+        {
+        }
+
+
+        //B4-P9/25. For_Matrix10x10
+        public static void B4_P9_25_For_Matrix10x10()
+        {
+           
+        }
+
+
+        //B4-P10/25. For_HelloWorld
+        public static void B4_P10_25_For_HelloWorld()
+        {
+        }
+
+
+        //B4_P11/25. For_Afrochildren
+        public static void B4_P11_25_For_Afrochildren()
+        {
+            
+        }
+
+
+        //B4-P12/25. For_Minus10OddEven
+        public static void B4_P12_25_For_Minus10OddEven()
+        {
+        }
+
+
+        //B4-P13/25 For_LettersCount
+        public static void B4_P13_25_For_LettersCount()
+        {
+            
+        }
+
+
+        //B4-P14/25 *For_AlphabetBack
+        public static void B4_P14_25_For_AlphabetBack()
+        {
+            
+        }
+
+
+        //B4-P15/25 While_OddEventNumber
+        public static void B4_P15_25_While_OddEventNumber()
+        {
+            
+        }
+
+
+        //B4-P16/25 DoWhile_OddEventNumber
+        public static void B4_P16_25_DoWhile_OddEventNumber()
+        {
+           
+        }
+
+
+        //B4-P17/25 While_HelloWorld
+        public static void B4_P17_25_While_HelloWorld()
+        {
+        }
+
+
+        //B4-P18/25 While_Multiplier
+        public static void B4_P18_25_While_Multiplier()
+        {
+            
+        }
+
+
+        //B4-P19/25 While_SolveNumberAdding
+        public static void B4_P19_25_While_SolveNumberAdding()
+        {
+            
+        }
+
+
+        //B4-P20/25 While_DiceGame
+        public static void B4_P20_25_While_DiceGame()
+        {
+            
+        }
+
+
+        //B4-P21/25 *While_DiceGameMultiplePlayers
+        public static void dB4_P21_25_While_DiceGameMultiplePlayers()
+        {
+        }
+
+
+        //B4-P22_25 *While_Akinator100Numbers
+        public static void B4_P22_25_While_Akinator100Numbers()
+        {
+            
+        }
+
+
+        //B4-P23/25 IfElse_Calcultor
+        public static void B4_P23_25_IfElse_Calcultor()
+        {
+            
+        }
+
+
+        //B4-P24_25 Switch_Calculator
+        public static void B4_P24_25_Switch_Calculator()
+        {
+            
+        }
+
+
+        //B4-P25/25 Cycle_WordRevercse
+        public static void B4_P25_25_Cycle_WordRevercse()
+        {
+        }
+    }
+}
